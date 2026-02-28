cdc9306a8a499e505d96ea21ef5a1b7e5649560a
Aditya Singh <aadi21082003@gmail.com>
2026-03-01 01:18:55 +0530

    deploy

--- frontend/src/pages/AdminLogin.jsx
+++ frontend/src/pages/AdminLogin.jsx
@@ -6,7 +6,7 @@ const AdminLogin = () => {
   const handleLogin = async (e) => {
     e.preventDefault();
 
-    const res = await fetch("http://localhost:5000/api/auth/login", {
+    const res = await fetch("http://locafyn.onrender.com/api/auth/login", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
